d1cc9c808fe41747301e228504acbe2e83923564
Halle <francrv@hotmail.com>
2021-11-21 16:49:03 -0500

    chore: refactor add controller spec unit test

--- spec/request/books_spec.rb
+++ spec/request/books_spec.rb
@@ -44,6 +44,7 @@ describe 'Books Api', type: :request do
         ]
       )
     end
+
     it 'returns a subset of books based on limit and offset' do
       get '/api/v1/books', params: { limit: 1, offset: 1 }
       expect(response).to have_http_status(:success)
